6711c460138b337a9348b23964212ee9f3ec8cd3
Simen Kristiansen <simenbolstad@gmail.com>
2023-01-18 11:04:31 +0100

    Added gitignore

--- RandomSquares/VirtualScreenCell.cs
+++ RandomSquares/VirtualScreenCell.cs
@@ -2,5 +2,13 @@
 
 public class VirtualScreenCell
 {
-    
+    public bool Up { get; private set; }
+    public bool Down { get; private set; }
+    public bool Left { get; private set; }
+    public bool Right { get; private set; }
+
+    public char GetCharacter()
+    {
+        
+    }
 }
